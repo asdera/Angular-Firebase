f6c3e37e1c25cb23b0e3c7df909adda7ac6e0f2a
unknown <poiuytandrew@gmail.com>
2018-12-23 15:21:27 -0500

    gallery

--- Jamhacks/src/app/home/home.component.ts
+++ Jamhacks/src/app/home/home.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 // import * as p5 from 'p5';
 
+import * as JSZip from 'jszip';
+
+interface Picture {
+  url: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
-  constructor() { }
+  archive: Picture[] = [];
+  zip: JSZip = JSZip();
+  constructor() {
+  }
 
   ngOnInit() {
+    console.log(this.archive)
+    for (var i=0; i<40; i++) {
+      this.archive.push({url: "/assets/archive/DSC_"+i+".JPG"});
+    }
+    console.log(this.archive)
   }
 
 }
